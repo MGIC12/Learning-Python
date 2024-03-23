2784db5fca4063af96b83d7d52edc2fc6ad8f737
MGIC12 <mager.infante@gmail.com>
2024-03-23 09:55:23 -0300

    Update if-else

--- if_else_elif.py
+++ if_else_elif.py
@@ -1,8 +1,36 @@
+#Using if-else
 rains = True
 temperature = 12
+print(f"Rains: {rains}")
+print(f"Temperature: {temperature}°C")
 if rains == True and temperature < 20:
     print("I'll use an umbrella and a coat")
 else:
     print("I don't need an umbrella or a coat")
 
 print("Now I'm going outside")
+
+print("------------------------------------------")
+
+#Using if
+puddle = True
+print(f"Puddle: {puddle}")
+print("Let's start walking")
+if puddle == True:
+    print("Let's jump on it!")
+
+print("I keep walking")
+
+print("------------------------------------------")
+
+#Using if-else in a better way
+rains = True
+print(f"Rains: {rains}")
+temperature = int(input("Temperature: "))
+if temperature < 18:
+    if rains == True:
+        print("I'll use an umbrella and a coat")
+    else:
+        print("I'll only use a coat")
+else:
+    print("I don't need an umbrella or a coat")
