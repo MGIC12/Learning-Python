ed98cbaff03633c53b2bc5bb73295bfb1f8c78b0
MGIC12 <mager.infante@gmail.com>
2024-03-23 09:33:26 -0300

    Added and

--- if_else_elif.py
+++ if_else_elif.py
@@ -1,7 +1,8 @@
 rains = True
-if rains == True:
-    print("I'll use an umbrella")
+temperature = 12
+if rains == True and temperature < 20:
+    print("I'll use an umbrella and a coat")
 else:
-    print("I won't use an umbrella")
+    print("I don't need an umbrella or a coat")
 
 print("Now I'm going outside")
