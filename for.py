1baff8f4a0f9f57adefa72e2a66c2a5799ab5c73
MGIC12 <mager.infante@gmail.com>
2024-03-24 11:46:26 -0300

    Modificado para explicar for con 3 parámetros

--- for.py
+++ for.py
@@ -1,5 +1,7 @@
 # range(x) 
 # Range crea una secuencia de números que parte desde 0 hasta el x-1
+# Si range tiene dos números (x, y) se representa como range(inicio, fin)
+# Y si range tiene 3 números como parámetros (x, y, c) se representa como range(inicio, fin , de cuanto en cuanto)
 print("f°      c°")
-for tempf in range(14, 30):
+for tempf in range(100, 110, 9):
     print(tempf,"      ",int((tempf)*5/9))
